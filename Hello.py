f0755de452a2e501e42d91654299258e30519cb6
gspoornima16 <gspoornima16@gmail.com>
2026-06-14 20:10:40 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -12,3 +12,9 @@ print(f'The intern is {Name} and {Age} years old and from chengalpattu')
 #this is a process to understand git cycle
 
 #using git pull
+
+integer=5  #can be negative or positive
+float=3.5 #decimal
+string="Handle with care"
+boolean=True #False too
+
