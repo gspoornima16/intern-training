51ccaa0cf0ff431967a744c8ea64724989621bbe
gspoornima16 <gspoornima16@gmail.com>
2026-06-19 18:52:34 +0530

    Add full CRUD REST API - Week 2 complete

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,HTTPException
 app = FastAPI()
 @app.get("/")
 def home():
@@ -9,3 +9,28 @@ def greet(name):
 @app.get("/multiply")
 def multiply(a:int,b:int):
     return{"ANS":a*b}
+Studentslist={"Aiden":"001","Bella":"002","Catherine":"003","David":"004"}
+@app.get("/students")
+def students():
+    return Studentslist
+@app.get("/students/{name}")
+def find_student(name):
+    if name not in Studentslist:
+        raise HTTPException
+    return f'{name}:{Studentslist[name]}'
+@app.post("/students/{name}")
+def add_student(name,id):
+    Studentslist[name] = id     
+    return Studentslist
+@app.put("/students/{name}")
+def update_data(name:str,id:int):
+    if name not in Studentslist:
+        raise HTTPException
+    Studentslist[name] = id
+    return {"updated list":Studentslist}
+@app.delete("/students/{name}")
+def del_data(name):
+    if name not in Studentslist:
+        raise HTTPException(status_code=404)
+    del Studentslist[name]
+    return Studentslist
