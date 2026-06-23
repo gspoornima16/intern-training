2ad55c2620114a840eb98b9d95a594be46f7ba3d
gspoornima16 <gspoornima16@gmail.com>
2026-06-23 18:43:14 +0530

    changed

--- fast-api/main.py
+++ fast-api/main.py
@@ -1,35 +1,29 @@
 from fastapi import FastAPI,HTTPException
+from pydantic import BaseModel
 app = FastAPI()
-@app.get("/")
-def home():
-    return {"message": "Welcome you all !"}
-@app.get("/greet/{name}")
-def greet(name):
-    return {"message": f"Hello, {name}!"}
-@app.get("/multiply")
-def multiply(a:int,b:int):
-    return{"ANS":a*b}
+class Student(BaseModel):
+    id: int
 Studentslist={"Aiden":"001","Bella":"002","Catherine":"003","David":"004"}
 @app.get("/students")
 def students():
     return Studentslist
 @app.get("/students/{name}")
-def find_student(name):
+def find_student(name:str):
     if name not in Studentslist:
         raise HTTPException
     return f'{name}:{Studentslist[name]}'
 @app.post("/students/{name}")
-def add_student(name,id):
+def add_student(name:str,id:int):
     Studentslist[name] = id     
     return Studentslist
 @app.put("/students/{name}")
 def update_data(name:str,id:int):
     if name not in Studentslist:
-        raise HTTPException
+        raise HTTPException(status_code=422)
     Studentslist[name] = id
     return {"updated list":Studentslist}
 @app.delete("/students/{name}")
-def del_data(name):
+def del_data(name:str):
     if name not in Studentslist:
         raise HTTPException(status_code=404)
     del Studentslist[name]
